8649ca93083e5fe292f2eddddb149fe5deac63ef
Dima Sergeev <web.sergeev.dima@gmail.com>
2023-02-21 07:17:00 +0300

    add header and tehnologies components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import Header from './Header.js';
+import Tehnologies from './Tehnologies.js';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            Hello, samurai! Let's go!
+            <Tehnologies/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Tehnologies.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Tehnologies = () => {
+    return (
+        <ul>
+            <li>Html</li>
+            <li>Css</li>
+            <li>Js</li>
+            <li>React</li>
+        </ul>
+    );
+}
+
+export default Tehnologies;
