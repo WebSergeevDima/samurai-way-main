bbd2eea7d066b599c21eafe0afd00941ec182553
Dima Sergeev <web.sergeev.dima@gmail.com>
2023-03-10 20:33:02 +0300

    21: add dialog html

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,7 +3,24 @@ import s from './Dialogs.module.css';
 
 const Dialogs: React.FC = () => {
     return (
-        <div>Dialog</div>
+        <div>
+
+            <div>Dialog</div>
+
+            <div className={s.dialogs}>
+                <div className={s.dialogsItems}>
+                    <div className={`${s.dialog} ${s.active}`}>11</div>
+                    <div className={s.dialog}>11</div>
+                    <div className={s.dialog}>11</div>
+                </div>
+                <div className={s.dialogsMessages}>
+                    <div className={s.message}>22</div>
+                    <div className={s.message}>22</div>
+                    <div className={s.message}>22</div>
+                </div>
+            </div>
+
+        </div>
     );
 }
 
