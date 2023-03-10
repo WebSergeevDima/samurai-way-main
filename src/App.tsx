28b3c6a0e24061738eb12f3c56dfae38ad54c6ef
Dima Sergeev <web.sergeev.dima@gmail.com>
2023-03-10 20:43:40 +0300

    22: route exact

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
                 <Navbar/>
                 <div className={'app-content'}>
                     <Route path={'/profile'} component={Profile}/>
-                    <Route path={'/dialogs'} component={Dialogs}/>
+                    <Route exact path={'/dialogs'} component={Dialogs}/>
                     <Route path={'/music'} component={Music}/>
                     <Route path={'/news'} component={News}/>
                     <Route path={'/setting'} component={Setting}/>
--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import s from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 const Dialogs: React.FC = () => {
     return (
@@ -9,9 +10,10 @@ const Dialogs: React.FC = () => {
 
             <div className={s.dialogs}>
                 <div className={s.dialogsItems}>
-                    <div className={`${s.dialog} ${s.active}`}>11</div>
-                    <div className={s.dialog}>11</div>
-                    <div className={s.dialog}>11</div>
+                    <NavLink activeClassName={`${s.active}`} to={'/dialogs/1'} className={`${s.dialog}`}>1</NavLink>
+                    <NavLink activeClassName={`${s.active}`} to={'/dialogs/2'} className={s.dialog}>2</NavLink>
+                    <NavLink activeClassName={`${s.active}`} to={'/dialogs/3'} className={s.dialog}>3</NavLink>
+                    <NavLink activeClassName={`${s.active}`} to={'/dialogs/4'} className={s.dialog}>4</NavLink>
                 </div>
                 <div className={s.dialogsMessages}>
                     <div className={s.message}>22</div>
