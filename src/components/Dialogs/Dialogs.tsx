849a1c8d82a94357dbfdf42057139e9894398c1b
Dima Sergeev <web.sergeev.dima@gmail.com>
2023-03-10 20:58:07 +0300

    23: props

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,6 +2,29 @@ import React from "react";
 import s from './Dialogs.module.css';
 import {NavLink} from "react-router-dom";
 
+type DialogItemPropsType = {
+    name: string
+    id: string
+}
+
+const DialogItem: React.FC<DialogItemPropsType> = (props) => {
+    return (
+      <div>
+          <NavLink activeClassName={`${s.active}`} to={'/dialogs/' + props.id} className={`${s.dialog}`}>{props.name}</NavLink>
+      </div>
+    );
+}
+
+type MessagePropsType = {
+    message: string
+}
+
+const Message: React.FC<MessagePropsType> = (props) => {
+    return (
+      <div className={s.message}>{props.message}</div>
+    );
+}
+
 const Dialogs: React.FC = () => {
     return (
         <div>
@@ -10,15 +33,16 @@ const Dialogs: React.FC = () => {
 
             <div className={s.dialogs}>
                 <div className={s.dialogsItems}>
-                    <NavLink activeClassName={`${s.active}`} to={'/dialogs/1'} className={`${s.dialog}`}>1</NavLink>
-                    <NavLink activeClassName={`${s.active}`} to={'/dialogs/2'} className={s.dialog}>2</NavLink>
-                    <NavLink activeClassName={`${s.active}`} to={'/dialogs/3'} className={s.dialog}>3</NavLink>
-                    <NavLink activeClassName={`${s.active}`} to={'/dialogs/4'} className={s.dialog}>4</NavLink>
+                    <DialogItem name="Dima" id="1"/>
+                    <DialogItem name="Dima2" id="2"/>
+                    <DialogItem name="Dima3" id="3"/>
+                    <DialogItem name="Dima4" id="4"/>
+                    <DialogItem name="Dima5" id="5"/>
                 </div>
                 <div className={s.dialogsMessages}>
-                    <div className={s.message}>22</div>
-                    <div className={s.message}>22</div>
-                    <div className={s.message}>22</div>
+                    <Message message={'Hello!'}/>
+                    <Message message={'Hello2222!'}/>
+                    <Message message={'Hello333333!'}/>
                 </div>
             </div>
 
