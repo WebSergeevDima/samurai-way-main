c2529db76f8ecf5b4a6b472e60bb754e03f1895a
Dima Sergeev <web.sergeev.dima@gmail.com>
2023-03-10 20:10:43 +0300

    19: add route

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,26 @@ import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
 import s from "./components/Profile/Profile.module.css";
+import {BrowserRouter, Route} from 'react-router-dom';
+import Music from "./components/Music/Music";
+import News from "./components/News/News";
+import Setting from "./components/Setting/Setting";
 
 function App() {
     return (
-        <div className="app-wrapper">
-            <Header/>
-            <Navbar/>
-            <div className={'app-content'}>
-                {/*<Profile />*/}
-                <Dialogs/>
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <Navbar/>
+                <div className={'app-content'}>
+                    <Route path={'/profile'} component={Profile}/>
+                    <Route path={'/dialogs'} component={Dialogs}/>
+                    <Route path={'/music'} component={Music}/>
+                    <Route path={'/news'} component={News}/>
+                    <Route path={'/setting'} component={Setting}/>
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 }
 
---
+++ src/components/Music/Music.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import s from './Music.module.css';
+
+const Music: React.FC = () => {
+    return (
+        <div>Music</div>
+    );
+}
+
+export default Music;
