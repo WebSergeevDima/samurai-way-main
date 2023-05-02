e56f924365b435f34dbd503f30329d88faf81744
Dima Sergeev <web.sergeev.dima@gmail.com>
2023-05-02 23:07:08 +0300

    add lesson 51

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,18 +1,37 @@
 import React from 'react';
 import style from './Users.module.css';
+import axios from "axios";
+
+
+const KEY = '523d0f3c-d82a-44a9-a924-328d6a4a9c04';
+
+export const instance = axios.create({
+    withCredentials: true,
+    headers:     {
+        "API-KEY": KEY
+    }
+});
 
 
 const Users = (props:any) => {
 
-    if(props.users.length === 0) {
+    axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
 
-        props.setUsers([
-            {id: 1, photo: 'https://www.interfax.ru/ftproot/photos/photostory/2021/06/11/week4_1100.jpg', isFollow: true, fullName: 'Name 1', status: 'My status', location: {city: 'Moscow', country: 'Russia'}},
-            {id: 2, photo: 'https://www.interfax.ru/ftproot/photos/photostory/2021/06/11/week4_1100.jpg', isFollow: true, fullName: 'Name 2', status: 'My status 2', location: {city: 'Moscow 2', country: 'Russia 2'}},
-            {id: 3, photo: 'https://www.interfax.ru/ftproot/photos/photostory/2021/06/11/week4_1100.jpg', isFollow: false, fullName: 'Name 3', status: 'My status 3', location: {city: 'Moscow 3', country: 'Russia 3'}},
-        ]);
+        if(response.data.items === 0) {
+            props.setUsers(response.data.items);
+        }
 
-    }
+    });
+
+    // if(props.users.length === 0) {
+    //
+    //     props.setUsers([
+    //         {id: 1, photo: 'https://www.interfax.ru/ftproot/photos/photostory/2021/06/11/week4_1100.jpg', isFollow: true, fullName: 'Name 1', status: 'My status', location: {city: 'Moscow', country: 'Russia'}},
+    //         {id: 2, photo: 'https://www.interfax.ru/ftproot/photos/photostory/2021/06/11/week4_1100.jpg', isFollow: true, fullName: 'Name 2', status: 'My status 2', location: {city: 'Moscow 2', country: 'Russia 2'}},
+    //         {id: 3, photo: 'https://www.interfax.ru/ftproot/photos/photostory/2021/06/11/week4_1100.jpg', isFollow: false, fullName: 'Name 3', status: 'My status 3', location: {city: 'Moscow 3', country: 'Russia 3'}},
+    //     ]);
+    //
+    // }
 
 
     return (
@@ -25,8 +44,8 @@ const Users = (props:any) => {
                             {item.isFollow ? <button onClick={()=>props.unfollow(item.id)}>UNFOLLOW</button> : <button onClick={()=>props.follow(item.id)}>FOLLOW</button>}
                         </div>
                         <div>
-                            <div>{item.fullName}</div>
-                            <div>{item.location.city}</div>
+                            <div>{item.name}</div>
+                            {/*<div>{item.location.city}</div>*/}
                         </div>
                     </div>
                 )
